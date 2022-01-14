c423f5fde72872cb3765d061fb99f8940c1c0d08
vvduth <ducthai060501@gmail.com>
2022-01-14 09:47:44 +0200

    Handle post and get

---
+++ app.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'natours' });
+// });
+// // .json auto set the content type to application/json
+
+// app.post('/', (req,res) => {
+//     res.send('You can post to this endpoint');
+// })
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req,res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    });
+});
+
+app.get('/api/v1/tours/:id', (req,res) => {
+    console.log(req.params);
+    const id = req.params.id * 1 ; // covert to number
+
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message : 'Invalid id',
+        })
+    }
+    const tour = tours.find(el => el.id === id);
+    res.status(200).json({
+        status: 'success',
+        data: {
+           tour: tour
+        }
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+}); // to start the server
