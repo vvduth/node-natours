0fcbfc2aabe78791aedc9fa1ada7cf676be0ca00
vvduth <ducthai060501@gmail.com>
2022-01-16 03:31:59 +0200

    Handle data with mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,60 +1,91 @@
+const { enabled } = require('../app');
 const Tour = require('./../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'Failed',
-      message: 'Missing name or price',
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
     });
   }
-  next();
-};
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1; // covert to number
-
-  // const tour = tours.find((el) => el.id === id);
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour: tour,
-  //   },
-  // });
 };
 
-exports.createTour = (req, res) => {
-  //console.log(req.body);
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({});
+    // newTour.save()
+    const newTour = await Tour.create(req.body);
+    //console.log(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id, { strict: false });
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTimeq);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTimeq,
-    // results: tours.length,
-    // data: {
-    //   tours: tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  const tours = await Tour.find();
+  try {
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours: tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,15 @@
-
 const express = require('express');
-const {getAllUsers, creatUSer, getUser, updateUser, deleteUser} = require('./../controllers/userController')
-const userRouter = express.Router() ;
+const {
+  getAllUsers,
+  creatUSer,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require('./../controllers/userController');
+const userRouter = express.Router();
 
-userRouter
-    .route('/')
-    .get(getAllUsers)
-    .post(creatUSer);
+userRouter.route('/').get(getAllUsers).post(creatUSer);
 
-userRouter
-    .route('/:id')
-    .get(getUser)
-    .patch(updateUser)
-    .delete(deleteUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
-module.exports = userRouter ;
+module.exports = userRouter;
