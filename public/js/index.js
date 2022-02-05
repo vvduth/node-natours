6b98441cc884b615dea379a3f4800f0d1083ab09
vvduth <ducthai060501@gmail.com>
2022-02-05 10:23:06 +0200

    found bug but cant fix it yet

--- public/js/index.js
+++ public/js/index.js
@@ -2,35 +2,33 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
-import { updateSettings } from './updateSetting';
+import { updateSettings } from './updateSettings';
+import { bookTour } from './stripe';
 
-//DOM
+// DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
 
-//Values
 
+// DELEGATION
 if (mapBox) {
-  const locations = JSON.parse(
-    document.getElementById('map').dataset.locations
-  );
+  const locations = JSON.parse(mapBox.dataset.locations);
   displayMap(locations);
 }
 
-if (loginForm) {
+if (loginForm)
   loginForm.addEventListener('submit', e => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
   });
-}
-if (logOutBtn) {
-  logOutBtn.addEventListener('click', logout);
-}
+
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
 if (userDataForm)
   userDataForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -39,14 +37,36 @@ if (userDataForm)
     form.append('email', document.getElementById('email').value);
     form.append('photo', document.getElementById('photo').files[0]);
     console.log(form);
+
     updateSettings(form, 'data');
   });
+
 if (userPasswordForm)
-  userPasswordForm.addEventListener('submit', e => {
+  userPasswordForm.addEventListener('submit', async e => {
     e.preventDefault();
     document.querySelector('.btn--save-password').textContent = 'Updating...';
+
     const passwordCurrent = document.getElementById('password-current').value;
     const password = document.getElementById('password').value;
     const passwordConfirm = document.getElementById('password-confirm').value;
-    updateSettings({ passwordCurrent, password, passwordConfirm }, 'password');
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
+
+if (bookBtn) {
+  alert('hey');
+  bookBtn.addEventListener('click', e => {
+    e.target.textContent = 'Processing...';
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
+  });
+} else {
+  alert('no hey');
+}
