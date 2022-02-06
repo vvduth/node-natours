cfc112cd5ca875c3e1391db30e935ac01c596554
vvduth <ducthai060501@gmail.com>
2022-02-06 11:08:52 +0200

    Payment

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8885,7 +8885,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35229" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46581" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -9062,4 +9062,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=bundle.js.map
+//# sourceMappingURL=/bundle.js.map
