b7191e4a46b6a92a37b19ea818c736a4578d5a96
vvduth <ducthai060501@gmail.com>
2022-02-06 14:57:10 +0200

    Reverting to <commit-id>

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,15 +1,18 @@
 const express = require('express');
+
 const bookingController = require('./../controllers/bookingController');
+
 const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
 router.use(authController.protect);
 
-router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+router
+  .route('/checkout-session/:tourId')
+  .get(bookingController.getCheckoutSession);
 
 router.use(authController.restrictTo('admin', 'lead-guide'));
-
 router
   .route('/')
   .get(bookingController.getAllBookings)
@@ -20,5 +23,4 @@ router
   .get(bookingController.getBooking)
   .patch(bookingController.updateBooking)
   .delete(bookingController.deleteBooking);
-
 module.exports = router;
