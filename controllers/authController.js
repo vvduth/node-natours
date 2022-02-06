c53f89270e8cd8a0b667185737ba477d9e382b2f
vvduth <ducthai060501@gmail.com>
2022-02-06 13:00:27 +0200

    Ready to dploy

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,7 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const Email = require('./../utils/email');
 
+
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,7 +9,7 @@ export const bookTour = async tourId => {
   try {
     //1) Get checkout session form API
     const session = await axios(
-      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
+      `/api/v1/bookings/checkout-session/${tourId}`
     );
     console.log(session);
     //2) Create checkout form + charge credit card
